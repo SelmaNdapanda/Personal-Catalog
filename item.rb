c08cba7aaf998beb3d54841b84f4dc1686474d04
Selma <selmandapanda@yahoo.com>
2023-01-23 16:42:01 +0200

    Update item class

--- item.rb
+++ item.rb
@@ -1,6 +1,5 @@
 class Item
-  attr_reader :genre, :author, :source, :label
-  attr_accessor :publish_date, :archived
+  attr_accessor :genre, :author, :source, :label, :publish_date, :archived
 
   def initialize(publish_date)
     @id = Random.rand(1..1000)

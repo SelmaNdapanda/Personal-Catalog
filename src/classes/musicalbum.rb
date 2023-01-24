898ab5afa498096239e3cec3a89e88f8fee1e222
Yasir Khan <khanyaser007@gmail.com>
2023-01-24 21:10:22 +0500

    Fix linter error

--- src/classes/musicalbum.rb
+++ src/classes/musicalbum.rb
@@ -5,7 +5,7 @@ class MusicAlbum < Item
   attr_accessor :on_spotify
 
   def initialize(on_spotify, publish_date, archived: false, id: nil)
-    super(publish_date)
+    super(id, publish_date, archived: archived)
     @on_spotify = on_spotify
   end
 

a607d801caeedb5c3931849575c8055afa9ffd7a
Selma <selmandapanda@yahoo.com>
2023-01-24 16:14:49 +0200

    Add Game and Author methods

--- author.rb
+++ author.rb
@@ -7,4 +7,9 @@ class Author
     @last_name = last_name
     @items = []
   end
+
+  def add_items(item)
+    @items << item
+    item.author = self
+  end
 end
--- game.rb
+++ game.rb
@@ -1,4 +1,5 @@
 require_relative './item'
+require 'date'
 
 class Game < Item
   attr_accessor :multiplayer, :last_played_at, :publish_date, :archived
@@ -7,5 +8,11 @@ class Game < Item
     super(publish_date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    last_played_date = DateTime.parse(@last_played_at).to_date
+    archived = (Date.today.year - last_played_date.year) > 2
+    super && archived
   end
 end

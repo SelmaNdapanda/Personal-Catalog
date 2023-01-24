8785488bb29a519fae2da72e8a09bfd48c334e7a
blase147 <solarmails2@gmail.com>
2023-01-24 17:39:00 +0100

    Update movie.rb

--- movie.rb
+++ movie.rb
@@ -1,10 +1,11 @@
 require_relative './item'
 
 class Movie < Item
-  attr_reader true
+  attr_reader :id
+  attr_accessor :silent
 
-  def initialize
-    super(silent: true)
-    @silent = true
+  def initialize(silent, publish_date, archived: false)
+    super(id, publish_date, archived: archived)
+    @silent = silent
   end
 end

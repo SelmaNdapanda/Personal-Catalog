5507ff03c83c7518c6c4367c643bcfaab20fcaf8
Yasir Khan <khanyaser007@gmail.com>
2023-01-25 14:30:07 +0500

    Update item.rb file for tests

--- item.rb
+++ item.rb
@@ -1,19 +1,22 @@
+require 'date'
+
 class Item
   attr_accessor :genre, :author, :source, :label, :publish_date, :archived
 
-  @allitems = []
-
-  def initialize(published_date)
-    @id = Random.rand(1..1000)
-    @published_date = published_date
-    @archived = false
-    @allitems << self
+  def initialize(id, publish_date, archived: false)
+    @id = id.nil? ? Random.rand(1..1000) : id
+    @publish_date = Date.parse(publish_date)
+    @archived = archived
   end
 
   private
 
   def can_be_archived?
-    (Time.new.year - @publish_date) > 10
+    current_date = Date.today
+    year = current_date.year - @publish_date.year
+    return false if year < 10
+
+    true
   end
 
   def move_to_archive()

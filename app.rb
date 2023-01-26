887d37359c2694572ec0854efeabb04f791ef58e
blase147 <solarmails2@gmail.com>
2023-01-26 15:08:36 +0100

    Create methods for movie and source disply

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require_relative './src/classes/game'
 require_relative './src/menu_options/add_game'
 require_relative './src/menu_options/list_game'
 require_relative './src/menu_options/list_author'
+require_relative './src/menu_options/list_movie'
+require_relative './src/menu_options/list_source'
 
 class App
   attr_accessor :games, :books, :musics, :movies
@@ -10,6 +12,8 @@ class App
   def initialize
     @games = []
     @authors = []
+    @movies = []
+    @sources = []
   end
 
   def show_menu
---
+++ src/menu_options/list_movies.rb
@@ -0,0 +1,10 @@
+require_relative '../classes/movie'
+
+@movies = []
+
+def list_movies
+  puts 'MOVIES:'
+  @movies.each do |movie|
+    puts "Silent: #{movie.true}"
+  end
+end

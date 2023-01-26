7b6cf64fec18a78f65d70029879805e6b9947000
Samuellekpor <samuellekpor@gmail.com>
2023-01-26 07:03:16 +0000

    Correct Book class methods

--- src/classes/book.rb
+++ src/classes/book.rb
@@ -3,7 +3,7 @@ require '../../item'
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publish_date, archived: false, publisher, cover_state)
+  def initialize(publish_date, publisher, cover_state, archived: false)
     super(publish_date, archived)
     @publisher = publisher
     @cover_state = cover_state

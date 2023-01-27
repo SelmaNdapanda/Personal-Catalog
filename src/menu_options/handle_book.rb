d27e1ea4198fdc71612baf2cb8ca4b78585c3cda
Samuellekpor <samuellekpor@gmail.com>
2023-01-27 06:12:58 +0000

    Add method for adding a new book

--- src/menu_options/handle_book.rb
+++ src/menu_options/handle_book.rb
@@ -7,3 +7,21 @@ def list_books
     puts "Publisher: #{book.publisher}, Cover state: #{book.cover_state}, Published date: #{book.publish_date}"
   end
 end
+
+def add_book
+  puts 'Please enter the title of the label of the book :'
+  title = gets.chomp
+  puts 'Please enter the color of the label :'
+  color = gets.chomp
+  puts 'Please enter the name of the publisher :'
+  publisher = gets.chomp
+  puts 'Please enter the date of publication'
+  publish_date = gets.chomp
+  puts 'What is the state of the cover ?'
+  cover_state = gets.chomp
+  book = Book.new(publish_date, publisher, cover_state)
+  @labels << label = Label.new(title, color)
+  @books << book
+  label.add_item(book)
+  puts 'Label and Book created successfully!'
+end

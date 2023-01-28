d5b149b918b2bc2038cb405cc8b75d96e67c0952
Selma <selmandapanda@yahoo.com>
2023-01-28 08:10:05 +0200

    Delete Game and Author files

--- src/menu_options/list_author.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../classes/author'
-
-def list_authors
-  puts 'AUTHORS:'
-  @authors.each do |author|
-    puts "First Name: #{author.first_name}, Last Name: #{author.last_name}"
-  end
-end
